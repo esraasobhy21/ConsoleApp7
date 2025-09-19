dd24706c59f7ed379de2940dc89d060511158c76
esraa <esraa@ESRAA>
2025-09-20 02:48:34 +0300

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("enter your num");
+        int x = int.Parse(Console.ReadLine());
+        Console.WriteLine(x);
+
+
+
+    }
+}
